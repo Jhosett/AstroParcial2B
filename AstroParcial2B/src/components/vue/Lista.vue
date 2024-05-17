0202fc4d4d046116046e8e8c99576582668fbcfd
Jhosett <darwinjhosettbermudezromero@gmail.com>
2024-05-17 14:41:50 -0500

    Tratando de hacer el ejercicio :,c

--- AstroParcial2B/src/components/vue/Lista.vue
+++ AstroParcial2B/src/components/vue/Lista.vue
@@ -1,22 +1,29 @@
 <script setup>
 import { ref } from 'vue';
 
-const arrayList = ref(['Tema 1', 'Tema 2', 'Tema 3', 'Tema 4'])
+const cont = ref(0)
+const arrayList = [{tittle:'Tema 1',context:'Texto del tema 1'},{tittle:'Tema 2',context:'Texto del tema 2'},{tittle:'Tema 3',context:'Texto del tema 3'},{tittle:'Tema 4',context:'Texto del tema 4'}]
 
+function selectCage(arrList, index) {
+    
+    isClicked.value = !isClicked
+
+}
 
 </script>
 
 <template>
 
-    <section class="">
+    <section class="bg-blue-600 w-[500px] m-auto mt-10 p-[7px] flex flex-col">
 
-        <div class="bg-blue-600 w-[500px] m-auto mt-10 p-[6px] flex flex-col">
-            <div class="bg-blue-200 py-1 px-4 flex justify-between items-center w-[100%] mb-2 text-blue-900">
-                <p class="font-semibold text-lg">Tema 1</p>
-                <button class="text-2xl font-bold">+</button>
-            </div>
-            <div class="bg-blue-100 py-4 px-4 text-blue-900">
-                <p class="font-semibold text-sm">Texto del tema 4</p>
+        <div v-for="(arrList, index) in arrayList">
+            <button @click="selectCage" class="bg-blue-200 py-1 px-4 flex justify-between items-center w-[100%] mb-1 text-blue-900"
+                :class="{'bg-blue-900 text-white': !isClicked}">
+                <p class="font-semibold text-lg">{{ arrList.tittle }}</p>
+                <p class="text-2xl font-bold">+</p>
+            </button>
+            <div class="bg-blue-100 py-4 px-4 text-blue-900 mb-2">
+                <p class="font-semibold text-sm">{{ arrList.context }}</p>
             </div>
         </div>
 
