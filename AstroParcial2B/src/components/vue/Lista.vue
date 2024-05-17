caf36b12e80999da0d081cb4ab78f5d5032c7062
Jhosett <darwinjhosettbermudezromero@gmail.com>
2024-05-16 21:18:33 -0500

    Dejando espacio de practica

---
+++ AstroParcial2B/src/components/vue/Lista.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref } from 'vue';
+
+
+</script>
+
+<template>
+
+    <section class="m-auto">
+
+        <div class="bg-blue-800 w-[500px]">
+            <div></div>
+        </div>
+
+    </section>
+
+</template>
