cdd4372a6f53a4dd932fcd2b718b14fe74368abc
Jhosett <darwinjhosettbermudezromero@gmail.com>
2024-05-25 15:25:13 -0500

    Dejando espacio para terminar correo

--- AstroParcial2B/src/components/vue/Correo.vue
+++ AstroParcial2B/src/components/vue/Correo.vue
@@ -3,13 +3,20 @@ import { ref } from 'vue';
 import CheckedIcon from './CheckedIcon.vue'
 
 
+const mensajes = [
+    {tittle:'Correo 1',conten:'Texto del correo 1',time:'Hace una hora'},
+    {tittle:'Correo 2',conten:'Texto del correo 2',time:'Hace 5 minutos'},
+    {tittle:'Correo 3',conten:'Texto del correo 3',time:'Hace 3 horas'},
+    {tittle:'Correo 4',conten:'Texto del correo 4',time:'Hace 30 minutos'}
+]
+
 
 </script>
 
 <template>
 
     <div class="bg-slate-200 max-w-[500px] mx-auto mt-10 p-3">
-        <div class="w-full bg-white p-1" >
+        <div v-for="(messages,index) in mensajes" :key="index" class="w-full bg-white p-1 my-2 hover:cursor-pointer" >
             <p class="text-end text-xs font-semibold text-gray-400 h-0" >Marcar como leído</p>
             <div class="flex items-center m-1" >
                 <CheckedIcon class="ml-2 mr-4 text-green-500 text-xl" />
