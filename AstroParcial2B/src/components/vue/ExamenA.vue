a44fa22dddb4f59806b2ac3e06f3a7c3cdaed37d
Jhosett <darwinjhosettbermudezromero@gmail.com>
2024-06-20 17:13:34 -0500

    Preparando espacio para examenes

---
+++ AstroParcial2B/src/components/vue/ExamenA.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HomeIcon from "./HomeIcon.vue"
+
+
+</script>
+
+<template>
+    <section>
+        <div class="mx-auto p-4 my-[20%] w-[400px] bg-gray-200" >
+            <div class="flex items-center gap-2 hover:cursor-pointer hover:font-bold" >
+                <HomeIcon/>
+                <p>Home</p>
+            </div>
+        </div>
+    </section>
+</template>
